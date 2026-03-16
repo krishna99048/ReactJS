6287cd561f640a05d0977b5c6128227ec6a59393
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-03-16 17:19:04 +0530

    two-binding

--- two-binding/src/App.jsx
+++ two-binding/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import LogIn from './Components/LogIn'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      {/* Two way binding : manage form filed using usestate */}
+      <LogIn />
+    </>
   )
 }
 
